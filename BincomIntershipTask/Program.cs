b3b60dbd4c260a68bc33ad33f31c050c356b11f4
legiomaria <chikakool2010@gmail.com>
2024-09-06 23:06:50 +0200

    Task 1 completed

--- BincomIntershipTask/Program.cs
+++ BincomIntershipTask/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Task 1: A string variable named name with the value "John Doe"
-
 string name = "Joe Doe";
 Console.WriteLine(name);
+
+// Task 2: An integar variable named age with the value
+int age = 25;
+Console.WriteLine(age);
