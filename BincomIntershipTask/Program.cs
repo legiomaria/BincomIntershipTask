eb32c1c95a62f473a70b5640407ae7f3da747251
legiomaria <chikakool2010@gmail.com>
2024-09-06 23:02:16 +0200

    Task 1 completed

--- BincomIntershipTask/Program.cs
+++ BincomIntershipTask/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 1: A string variable named name with the value "John Doe"
+
+string name = "Joe Doe";
+Console.WriteLine(name);
