3aa3e117c749aba9a464cd29817fc8f8ed1b7f81
legiomaria <chikakool2010@gmail.com>
2024-09-06 23:12:18 +0200

    Task 1 completed

--- BincomIntershipTask/Program.cs
+++ BincomIntershipTask/Program.cs
@@ -5,3 +5,7 @@ Console.WriteLine(name);
 // Task 2: An integar variable named age with the value
 int age = 25;
 Console.WriteLine(age);
+
+// Task1 A boolean variable named isAdmin with the value true.
+bool isAdmin = true;
+Console.WriteLine(isAdmin);
